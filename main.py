da8901e890e309f54a8027104221dd20f07b372f
Jamison Sloan <jamison@plasmon.cumin.me>
2018-10-26 12:12:57 -0400

    Removing useless comments

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ from progress.bar import Bar
 import os, sys, time
 from benchmark import benchmark
 
-#jamison learns how to use github
-#comment
-
 class Simulation:
     def __init__(self, x_start=0, x_stop=1, number_of_psi=100, number_of_spatial_dimensions=1,
                  potential_function=lambda x: 0, non_linear=False, alpha=1e-12):
