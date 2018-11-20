Bug fix in numerical Jacobian

main.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     periodic_boundary_conditions = False
     gif_name = "trap_with_disturbance"
     time_start = 0
-    time_stop = 10
+    time_stop = 5
     delta_t = 2e-3
 
     animation_timestep = int(animation_interval / delta_t)
@@ -67,7 +67,7 @@ if __name__ == "__main__":
 
     # plt.plot(init_state)
 
-    init_state = 0.5*np.exp(-(NLSE.linspace**2)/2)
+    # init_state = 0.5*np.exp(-(NLSE.linspace**2)/2)
 
     # plt.plot(init_state)
     # plt.show()

problems.py
@@ -154,7 +154,7 @@ class NLSE(Problem):
     def calc_jacobian_numerical(self, f, x, u, p, epsilon):
         """Return the Jacobian calculated using finite-difference
         The Jacobian is size (n, 2n) where n is size of x because x is complex"""
-        jacobian = np.zeros((2*(self.number_of_psi-2), 2*(self.number_of_psi-2)), dtype=complex)
+        jacobian = np.zeros((self.number_of_psi-2, 2*(self.number_of_psi-2)), dtype=complex)
         f0 = f(x, u, p)
         # print(f0)
         for i in range(len(x)):